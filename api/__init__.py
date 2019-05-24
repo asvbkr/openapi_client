f3074723af29c886fc43b7177901872f57b8e20a
ASV <asv@bk.ru>
2019-05-24 10:16:09 +0300

    -

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 # flake8: noqa
 
-# import apis into api package etc
+# import apis into api package
 from openapi_client.api.bots_api import BotsApi
 from openapi_client.api.chats_api import ChatsApi
 from openapi_client.api.messages_api import MessagesApi
